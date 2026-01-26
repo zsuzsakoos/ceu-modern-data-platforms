bd406a28fe4b53dd304b218dba3d47155b72e7a4
Zsuzsi Koós <zsuzsi.koos@gamil.com>
2026-01-26 17:53:58 +0100

    class2 update

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -4,14 +4,16 @@
     on_schema_change='fail'
     )
 }}
+
 WITH src_reviews AS (
-    SELECT * FROM {{ ref('src_reviews') }}
+  SELECT * FROM {{ ref('src_reviews') }}
 )
-
-SELECT * FROM src_reviews
-WHERE
-    review_text IS NOT null
-
-    {% if is_incremental() %}
-        AND review_date > (SELECT max(review_date) FROM {{ this }})
-    {% endif %}
+SELECT
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+  *,
+  current_timestamp() AS loaded_at
+FROM src_reviews
+WHERE review_text is not null
+{% if is_incremental() %}
+  AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
---
+++ airbnb/tests/consistent_created_at.sql
@@ -0,0 +1,10 @@
+{{
+  config(
+    severity = 'warn'
+  )
+}}
+
+SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+INNER JOIN {{ ref('fct_reviews') }} r
+USING (listing_id)
+WHERE l.created_at <= r.review_date
